4a5583c8916efcd3f470a3003e50519dc9e12c8f
Vamsi8324 <vamsi8324@gmail.com>
2023-01-18 08:42:11 +0000

    question8

---
+++ Exam8/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        CourseRegistration registration = new CourseRegistration("Rupesh", 200, 41,1005,true);
+        registration.displayDetails();
+}}
